78a093397ddaa5e46ca954cac25a98d63efc0b5b
Shathi Islam <116778673+ssopno@users.noreply.github.com>
2022-12-18 19:05:08 +0600

    Create main.dart

---
+++ bin/main.dart
@@ -0,0 +1,51 @@
+////day2
+
+void main() {
+  /////dart number
+  var intNumber = 10;
+  print(intNumber);
+  var doubleNumber = 10.10;
+  print(doubleNumber);
+
+  ////dart String
+  var myStringSingle = 'This is single quoutes String';
+  var mystringDouble = "this is single quoutes string";
+  print(myStringSingle);
+  print(mystringDouble);
+
+  ////boolean
+  var negative = false;
+  bool positive = true;
+  print(negative);
+  print(positive);
+
+  ////dart List
+  var list = [1, 2, 3, 4];
+  print(list);
+
+  ////dart map
+  var map = {'name': 'shathi', 'age': 25, 'height': '5feet 3inch'};
+  print(map);
+
+////dart operators(arithmetics)
+  var n1 = 10;
+  var n2 = 5;
+  print("n1+n2=${n1 + n2}");
+  print("n1-n2=${n1 - n2}");
+  print("n1*n2=${n1 * n2}");
+  print("n1/n2=${n1 / n2}");
+  print("n1%n2=${n1 % n2}");
+
+  ////dart operators
+  var x = 30;
+  print(x++);
+
+  var y = 30;
+  print(++y);
+
+  var z = 10;
+  print(--z);
+
+  var u = 12;
+  print(u--);
+}
