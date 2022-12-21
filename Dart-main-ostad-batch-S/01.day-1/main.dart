a529b222524691726d568f11cd8be2fe08fbd962
Shathi Islam <116778673+ssopno@users.noreply.github.com>
2022-12-21 13:01:43 +0600

    Add files via upload

---
+++ Dart-main-ostad-batch-S/01.day-1/main.dart
@@ -0,0 +1,72 @@
+void main(){
+  print('Hello dart');
+
+
+///dart number
+   ///dart number
+
+      var intNumber=10;
+      var doubleNumbr=10.10;
+      print(intNumber);
+      print(doubleNumbr);
+
+      var myStringsingle='this is the single quotes string';
+      var mystringdouble="this is the double quotes string";
+      print(myStringsingle);
+      print(mystringdouble);
+
+
+
+  ///dart variable
+  ///type non sefty
+  var fname="shathi";
+  dynamic lname='Islam';
+  var age=25;
+
+  print(fname+' '+lname);
+  print(age);
+
+  ///type safty
+  ///String
+  String name1='Shathi';
+  print(name1);
+
+  ///integer
+  int age1=25;
+  print(age1);
+
+  ///double
+  double gpa=4.69;
+  print(gpa);
+
+  /// boolean for true / false
+  bool ans = false;
+  print(ans);
+  ///operators
+   int a = 82;
+   int b = 10;
+
+  int c = a + b;
+  print(c);
+
+  int d = a - b;
+  print(d);
+
+  int e = a * b;
+  print(e);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
