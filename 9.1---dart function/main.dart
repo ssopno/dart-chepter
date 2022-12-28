02afe3892c998ceee5b4ad49d0f627b32842c30f
Shathi Islam <116778673+ssopno@users.noreply.github.com>
2022-12-28 20:58:59 +0600

    Add files via upload

---
+++ 9.1---dart function/main.dart
@@ -0,0 +1,32 @@
+///dart function
+
+
+void main(){
+  myfunction();
+  addTwo("Shathi", "islam");
+  multiThree(10, 30, 20);
+
+
+
+}
+
+void myfunction(){
+  int a=10;
+  int b=20;
+  print('this is function');
+  print(a+b);
+
+
+}
+
+///function parapeter
+void addTwo(var a, var b){
+  print(a+" "+b);
+}
+void multiThree(var a, var b, var c){
+  print(a+b+c);
+}
+
+
+///function return & return type
+
