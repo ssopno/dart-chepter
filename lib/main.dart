3cc540e60b574e682eea8c57d0411d8ee031f4d0
Shathi Islam <116778673+ssopno@users.noreply.github.com>
2023-04-02 12:48:18 +0600

    getx

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:  MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+ RxInt _counter = 0.obs;
+
+  void _incrementCounter() {
+      _counter++;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Obx(() =>  Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            ),
+            ElevatedButton(
+                onPressed: (){
+                  // Navigator.push(context, MaterialPageRoute(builder:( context)=>const SecondScreen()));
+                  // Get.to(const SecondScreen());
+                  // Get.snackbar('shathi','fardin');
+                  Get.dialog(AlertDialog());
+                },
+                child:const  Text('go to second screen'))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:const Text('Second Screen'),
+      ),
+    ) ;
+  }
+}
+
+
+
