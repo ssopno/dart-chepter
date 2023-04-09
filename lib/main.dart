70bfa3d26981f751afe3226270efd85f5b69ae0e
Shathi Islam <116778673+ssopno@users.noreply.github.com>
2023-04-09 12:28:18 +0600

    firebase live class41

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-import 'dart:developer';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import '';
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -40,16 +39,17 @@ class _BookListScreenState extends State<BookListScreen> {
   @override
   void initState() {
     super.initState();
-    getAllBooks();
+    // getAllBooks();
   }
 
   Future<void> getAllBooks() async {
     _getBooksInProgress = true;
     setState(() {});
     books.clear();
-    await firebaseFirestore.collection('books').get().then((value) {
-      for(var doc in value.docs ){
-        books.add(BookList(doc.get('name'), doc.get('writter'),doc.get('year')));
+    await firebaseFirestore.collection('books').get().then((documents) {
+      for (var doc in documents.docs) {
+        books.add(
+            BookList(doc.get('name'), doc.get('writter'), doc.get('year')));
       }
     });
     _getBooksInProgress = false;
@@ -62,24 +62,41 @@ class _BookListScreenState extends State<BookListScreen> {
       appBar: AppBar(
         title: const Text('Book Collection'),
       ),
-      body: _getBooksInProgress
-          ? const Center(
-          child: CircularProgressIndicator()
-      )
-          : RefreshIndicator(
-        onRefresh:()async {
-          getAllBooks();
-        },
-            child: ListView.builder(
-                itemCount: books.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(books[index].name),
-                    subtitle: Text(books[index].writter),
-                    trailing: Text(books[index].year),
-                  );
-                }),
-          ),
+      body: StreamBuilder<QuerySnapshot>(
+          stream: firebaseFirestore.collection('books').snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(snapshot.error.toString()),
+              );
+            }
+            if (snapshot.hasData) {
+              books.clear();
+              for (var doc in snapshot.data!.docs) {
+                books.add(BookList(
+                    doc.get('name'), doc.get('writter'), doc.get('year')));
+              }
+
+              return ListView.builder(
+                  itemCount: books.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(books[index].name),
+                      subtitle: Text(books[index].writter),
+                      trailing: Text(books[index].year),
+                    );
+                  });
+            } else {
+              return const Center(
+                child: Text('No Data available'),
+              );
+            }
+          }),
     );
   }
 }
